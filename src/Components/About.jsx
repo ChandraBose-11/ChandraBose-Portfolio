e15000f03f873c9d5039c11b627f04b2a16ce94a
ChandraBose-11 <chandrubose46@gmail.com>
2025-04-25 20:54:02 +0530

    Skill Section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -20,6 +20,7 @@ import {
 
 import { DiVisualstudio } from "react-icons/di";
 import { IoLogoVercel } from "react-icons/io5";
+import { Card } from "flowbite-react";
 const About = () => {
   return (
     <div
@@ -52,88 +53,103 @@ const About = () => {
           <h2 className="text-2xl font-bold text-[#fe5617] text-center">
             My Skills
           </h2>
-
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-8 text-center">
-            {/* Skill Icons */}
-
-            <div className="flex flex-col items-center space-y-2">
+          {/* Frontend */}
+          <h2 className="text-2xl font-bold text-[#fe5617] text-center pb-2">
+            Front-End
+          </h2>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-8 text-center p-2 ">
+            <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <FaHtml5 size={40} className="text-[#e34c26]" />
-              <span className="font-bold">HTML5</span>
+              <span className="font-bold text-black dark:text-white">
+                HTML5
+              </span>
             </div>
-            <div className="flex flex-col items-center space-y-2">
+           <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <FaCss3Alt size={40} className="text-[#264de4]" />
               <span className="font-bold">CSS3</span>
             </div>
-            <div className="flex flex-col items-center space-y-2">
+           <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <FaJsSquare size={40} className="text-[#f7df1e]" />
               <span className="font-bold">JavaScript</span>
             </div>
-            <div className="flex flex-col items-center space-y-2">
+           <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <SiBootstrap
                 size={40}
                 className="text-[#563d7c] dark:text-blue-600"
               />
               <span className="font-bold">Bootstrap</span>
             </div>
-            <div className="flex flex-col items-center space-y-2">
+           <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <SiTailwindcss size={40} className="text-[#38bdf8]" />
               <span className="font-bold">Tailwind CSS</span>
             </div>
-            <div className="flex flex-col items-center space-y-2">
+           <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <FaReact size={40} className="text-[#61dbfb]" />
               <span className="font-bold">React.js</span>
             </div>
-
-            <div className="flex flex-col items-center space-y-2">
+          </div>
+          {/* Backend*/}
+          <h2 className="text-2xl font-bold text-[#fe5617] text-center mt-1">
+            Back-End
+          </h2>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-8 text-center">
+           <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <FaNode size={40} className="text-[#68a063]" />
               <span className="font-bold">Node.js</span>
             </div>
-            <div className="flex flex-col items-center space-y-2">
+           <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <SiExpress size={40} className="text-[#000000] " />
               <span className="font-bold">Express.js</span>
             </div>
-            <div className="flex flex-col items-center space-y-2">
+           <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <SiMongodb size={40} className="text-[#47a248]" />
               <span className="font-bold">MongoDB</span>
             </div>
-            <div className="flex flex-col items-center space-y-2">
+           <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <SiMysql size={40} className="text-[#000000] dark:text-white" />
               <span className="font-bold">MySQL</span>
             </div>
-
-            <div className="flex flex-col items-center space-y-2">
+          </div>
+          {/* Tools */}
+          <h2 className="text-2xl font-bold text-[#fe5617] text-center mt-1">
+            Tools
+          </h2>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-8 text-center">
+           <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <FaGitAlt size={40} className="text-[#f05032]" />
               <span className="font-bold">Git</span>
             </div>
-            <div className="flex flex-col items-center space-y-2">
+           <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <FaGithub size={40} className="text-black dark:text-white" />
               <span className="font-bold">GitHub</span>
             </div>
-            <div className="flex flex-col items-center space-y-2">
+           <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <DiVisualstudio size={40} className="text-[#0078d7]" />
               <span className="font-bold">VS Code</span>
             </div>
-            <div className="flex flex-col items-center space-y-2">
+           <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <SiPostman size={40} className="text-[#ef5b25]" />
               <span className="font-bold">Postman</span>
             </div>
 
-            <div className="flex flex-col items-center space-y-2">
+           <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <IoLogoVercel size={40} className="text-[#000] dark:text-white" />
               <span className="font-bold">Vercel</span>
             </div>
-            <div className="flex flex-col items-center space-y-2">
+           <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <SiNetlify size={40} className="text-[#00d1b2]" />
               <span className="font-bold">Netlify</span>
             </div>
-            <div className="flex flex-col items-center space-y-2">
+           <div className="flex flex-col items-center space-y-2 border-2 border-slate-800 rounded-2xl p-4 shadow-md bg-white dark:bg-slate-800 shadow-slate-100 border-3">
               <SiRender size={40} className="text-[#000] dark:text-white" />
               <span className="font-bold">Render</span>
             </div>
           </div>
+          {/* Skill Icons */}
         </div>
       </div>
     </div>
+    // </div>
   );
 };
 
